0a09462541f030ffbc240dada46a30e47e30a682
Miguel Coelho <miguelmendescoelho@gmail.com>
2018-12-03 22:42:44 +0000

    computeProb and Tests

--- BN.py
+++ BN.py
@@ -5,16 +5,28 @@ Created on Mon Oct 15 15:51:49 2018
 @author: mlopes
 """
 
-
+import numpy as np
 
 class Node():
     def __init__(self, prob, parents = []):
-        pass
+        self.parents = parents
+        self.prob = prob
+
     
     def computeProb(self, evid):
-        pass
+        if len(self.parents) == 0:
+            return 1-self.prob[0], self.prob[0]
 
-        return 0
+        tempProb = self.prob
+        tempParents = self.parents
+        evTemp = evid[tempParents[0]]
+
+        while (len(tempParents) > 1):
+            tempProb = tempProb[evTemp]
+            tempParents = tempParents[1:]
+            evTemp = evid[tempParents[0]]
+
+        return 1 - tempProb[evTemp], tempProb[evTemp]
     
 class BN():
     def __init__(self, gra, prob):
@@ -30,3 +42,6 @@ class BN():
         pass
         
         return 0
+
+
+
---
+++ Tests/test.py
@@ -0,0 +1,7 @@
+a = [[1, 2], [3, 4]]
+a = a[0]
+
+print( a, isinstance(a, list) )
+a = a[0]
+print( a, isinstance(a, list) )
+
