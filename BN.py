7173e781a6c26ef4a29f6bf5b5c99d3813bf71e8
Miguel Coelho <miguelmendescoelho@gmail.com>
2018-12-04 16:16:19 +0000

    computePostProb Done

--- BN.py
+++ BN.py
@@ -30,18 +30,109 @@ class Node():
     
 class BN():
     def __init__(self, gra, prob):
-        pass
+        self.gra = gra
+        self.prob = prob
 
     def computePostProb(self, evid):
-        pass
-               
-        return 0
-        
-        
+        evid = list(evid)
+        index = evid.index(-1)
+
+        evidAux = evid.copy()
+        evidAux[index] = 0
+        sum1=0
+        for ele in self.evComb(evidAux):
+            sum1 += self.computeJointProb(ele)
+
+        evidAux = evid.copy()
+        evidAux[index] = 1
+        sum2 = 0
+        for ele in self.evComb(evidAux):
+            sum2 += self.computeJointProb(ele)
+
+
+        alpha = 1/(sum1 + sum2)
+
+        return alpha * sum2
+
     def computeJointProb(self, evid):
-        pass
-        
-        return 0
+        temp = 1
+
+        for i, val in enumerate(self.prob):
+            temp *= val.computeProb(evid)[evid[i]]
+
+        return temp
+
+    def evCombAux(self,ev, i, res):
+        ev = list(ev)
+        index = ev.index([])
+        ev[index] = i
+        if [] not in ev:
+            res.append(ev)
+            return res
+        return self.evCombAux(ev, 0,res), self.evCombAux(ev, 1,res)
+
+    def evComb(self,ev):
+        res = []
+        self.evCombAux(ev, 0, res)
+        self.evCombAux(ev, 1, res)
+        return res
 
 
 
+
+#######################
+#        TESTS        #
+#######################
+
+
+gra = [[], [0], [0], [1, 2]]
+ev = (1, 1, 1, 1)
+
+p1 = Node(np.array([.5]), gra[0])  # cloudy
+print("p1 false %.4e p1 true %.4e" % (p1.computeProb(ev)[0], p1.computeProb(ev)[1]))
+
+p2 = Node(np.array([.5, .1]), gra[1])  # sprinkler
+
+p3 = Node(np.array([.2, .8]), gra[2])  # rain
+
+p4 = Node(np.array([[.0, .9], [.9, .99]]), gra[3])  # wetgrass
+print("p2 = 1, p3 = 1, p4 false %.4e p4 true %.4e" % (p4.computeProb(ev)[0], p4.computeProb(ev)[1]))
+
+prob = [p1, p2, p3, p4]
+
+bn = BN(gra, prob)
+
+jp = []
+for e1 in [0, 1]:
+    for e2 in [0, 1]:
+        for e3 in [0, 1]:
+            for e4 in [0, 1]:
+                jp.append(bn.computeJointProb((e1, e2, e3, e4)))
+
+print("sum joint %.3f (1)" % sum(jp))
+
+### Tests to joint Prob
+ev = (0, 0, 0, 0)
+print("joint %.4g (0.2)" % bn.computeJointProb(ev))
+
+ev = (1, 1, 1, 1)
+print("joint %.4g (0.0396)" % bn.computeJointProb(ev))
+
+### Tests to post Prob
+# P(e1|e4=1)
+ev = (-1, [], [], 1)
+print("ev : ")
+print(ev)
+print("post : %.4g (0.5758)" % bn.computePostProb(ev))
+
+# P(e4|e1=1)
+ev = (1, [], [], -1)
+print("ev : ")
+print(ev)
+print("post : %.4g (0.7452)" % bn.computePostProb(ev))
+
+# P(e1|e2=0,e3=0)
+ev = (-1, 0, 0, [])
+print("ev : ")
+print(ev)
+print("post : %.4g (0.3103)" % bn.computePostProb(ev))
--- Tests/test.py
+++ Tests/test.py
@@ -1,7 +1,56 @@
-a = [[1, 2], [3, 4]]
-a = a[0]
+import itertools
 
-print( a, isinstance(a, list) )
-a = a[0]
-print( a, isinstance(a, list) )
+# n=2
+# lst = map(list, itertools.product([0, 1], repeat=n))
+# # lst = list(itertools.product([0, 1], repeat=n))
+# for a in lst:
+#     print(a)
+#
+# result = [(a, 1, 2, a, 4) for a in map(list, itertools.product([0, 1], repeat=n))]
+#
+# print(result)
 
+# ev = list((-1,[],[],1,1))
+#
+# print(ev.index([]))
+# ev[ev.index([])] = 2
+# print(ev.index([]))
+# ev[ev.index([])] = 2
+# print(ev.index([]))
+
+# RECURSION WITH LINEAR LIST
+
+
+# def evCombAux(ev, i, res):
+#     ev = list(ev)
+#     index = ev.index([])
+#     ev[index] = i
+#     if [] not in ev:
+#         res.append(ev)
+#         return res
+#     return evCombAux(ev, 0,res), evCombAux(ev, 1,res)
+#
+# def evComb(ev):
+#     res = []
+#     evCombAux(ev, 0, res)
+#     evCombAux(ev, 1, res)
+#     return res
+
+
+# RECURSION WITH NESTED LISTS
+# def evCombAux(ev, i):
+#     ev = list(ev)
+#     index = ev.index([])
+#     ev[index] = i
+#     if [] not in ev:
+#         return ev
+#     return [evCombAux(ev, 0), evCombAux(ev, 1)]
+#
+# def evComb(ev):
+#
+#     return [evCombAux(ev, 0), evCombAux(ev, 1)]
+
+
+# ev = ([],[],[])
+# a =evComb(ev)
+# print(a)
